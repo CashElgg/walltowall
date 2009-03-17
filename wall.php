6d5d8cb06df5553871f66f3e3a538c9c23d70c56
cash.costello <cash.costello@gmail.com>
2009-03-17 02:11:42 +0000

    now populating sidebar

--- wall.php
+++ wall.php
@@ -5,22 +5,21 @@
    *
    * The wall to wall feature only shows the last ten messages between the two 
    * users (with the exception of the oddity of messages that they post on their
-   * own board). There is currently no pagination. There is no delete message 
-   * button when on wall to wall. I haven't tried to add any additional security 
-   * over the current messageboard and the current messageboard has little or no 
-   * security. This means anyone can view the messages if they click on "reply"  
-   * or "wall to wall" even if that person had made their messageboard private 
-   * or restricted to a collection of friends. 
+   * own board). There is no delete message button when on wall to wall. I haven't 
+   * tried to add any additional security over the current messageboard and the 
+   * current messageboard has little or no security. This means anyone can view 
+   * the messages if they click on "reply" or "wall to wall" even if that person 
+   * had made their messageboard private or restricted to a collection of friends. 
    *
    * @package ElggMessageBoard Walltowall Extension
-   * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+   * @license GNU Lesser General Public License version 3
    * @author Cash Costello
    */
 
   global $CONFIG;   
 
-  require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
-  require_once $CONFIG->pluginspath . 'walltowall/lib/walltowall_annotations.php';
+  include_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
+  include_once $CONFIG->pluginspath . 'walltowall/lib/walltowall_annotations.php';
 
   // set context for special handling of comments for wall to wall  
   $prev_context = get_context();
@@ -44,11 +43,22 @@
   $viewer = $_SESSION['guid'];
   
   // heading for wall to wall
-  $tmp_entity = get_entity($poster);
-  $poster_name = $tmp_entity->name;
-  $tmp_entity = get_entity($postee);
-  $postee_name = $tmp_entity->name;
+  $poster_entity = get_entity($poster);
+  $poster_name = $poster_entity->name;
+  $postee_entity = get_entity($postee);
+  $postee_name = $postee_entity->name;
   
+  // sidebar creation - reusing owner block divs even though they are ids and content_... because there is so little to work with in the css
+	$area1  = '<div id="content_area_user_title"><h2>' . elgg_echo('walltowall:title') . '</h2></div><br />';
+  $icon   = elgg_view("profile/icon",array('entity' => $poster_entity, 'size' => 'small'));
+  $area1 .= "<div id=\"owner_block_icon\">" . $icon . "</div>";
+	$info   = '<a href="' . $poster_entity->getURL() . '">' . $poster_entity->name . '</a>';
+	$area1 .= "<div id=\"owner_block_content\">" . $info . "</div>";  
+	$icon   = elgg_view("profile/icon",array('entity' => $postee_entity, 'size' => 'small'));
+  $area1 .= "<br /><div id=\"owner_block_icon\">" . $icon . "</div>";
+	$info   = '<a href="' . $postee_entity->getURL() . '">' . $postee_entity->name . '</a>';
+	$area1 .= "<div id=\"owner_block_content\">" . $info . "</div>";
+
   // special handling if viewer was part of conversation
   if ($viewer == $poster)
    $poster_name = elgg_echo('walltowall:me');
@@ -89,7 +99,7 @@
            
   $area2 .= $nav;
       
-  $body = elgg_view_layout("two_column_left_sidebar", '', $area2);
+  $body = elgg_view_layout("two_column_left_sidebar", $area1, $area2);
 
     
   page_draw(elgg_echo('walltowall:title'), $body);    
